8db46974330f917fc001363cb6a3ba10c82ee43c
ikivanc <ikivanc@IKIVANCL>
2016-04-20 16:22:13 +0300

    Project Architecture designed in VS Project Empty Template

---
+++ App1/HMLS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HMLS.Web.Startup))]
+namespace HMLS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
