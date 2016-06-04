9f2ab338664cf76fa64bed8c92c88bf2a1053d76
Ahmed Adel <ahmed_adel_ms@hotmail.com>
2016-06-04 12:54:47 +0400

    Test Comment

--- App1/AAAZureZoomConsole/Program.cs
+++ App1/AAAZureZoomConsole/Program.cs
@@ -12,11 +12,16 @@ namespace AAAZureZoomConsole
     {
         static void Main(string[] args)
         {
+            // Get all needed parms
             Console.WriteLine("Pls enter the URL of the video/audio file: ");
             string fileURL = Console.ReadLine();
+            // Upload the video file to Azure media services
             CreateAssetAndUploadSingleFile(AssetCreationOptions.None, "c:\\temp\\abiola.wmv");
             Console.WriteLine("Done! ;-) ");
             Console.ReadLine();
+            // Transcribe the video
+            // Search the transcription
+            // Hyperlapse the video around the kywords
 
         }
 
@@ -29,6 +34,7 @@ namespace AAAZureZoomConsole
                 return null;
             }
             */
+            // Test Comment
             var _context = new CloudMediaContext("aamediaservice1", "uIux27Hig96nRLkGZEZcareBz0x5t2gPPA533riDgTo=");
             var assetName = Path.GetFileNameWithoutExtension(singleFilePath);
             IAsset inputAsset = _context.Assets.Create(assetName, assetCreationOptions);
@@ -55,5 +61,83 @@ namespace AAAZureZoomConsole
             return inputAsset;
         }
     }
+
+    
+    // Check job execution and wait for job to finish. 
+    Task progressPrintTask = new Task(() =>
+    {
+        IJob jobQuery = null;
+        do
+        {
+            var progressContext = new CloudMediaContext(_accountName,
+                            _accountKey);
+            jobQuery = progressContext.Jobs.Where(j => j.Id == job.Id).First();
+            Console.WriteLine(string.Format("{0}\t{1}\t{2}",
+                  DateTime.Now,
+                  jobQuery.State,
+                  jobQuery.Tasks[0].Progress));
+            Thread.Sleep(10000);
+        }
+        while (jobQuery.State != JobState.Finished &&
+               jobQuery.State != JobState.Error &&
+               jobQuery.State != JobState.Canceled);
+    });
+    progressPrintTask.Start();
+
+    // Check job execution and wait for job to finish. 
+    Task progressJobTask = job.GetExecutionProgressTask(CancellationToken.None);
+    progressJobTask.Wait();
+
+    // If job state is Error, the event handling 
+    // method for job progress should log errors.  Here you check 
+    // for error state and exit if needed.
+    if (job.State == JobState.Error)
+    {
+        Console.WriteLine("Exiting method due to job error.");
+        return false;
+    }
+
+    // Download the job outputs.
+    DownloadAsset(task.OutputAssets.First(), outputFolder);
+
+    return true;
+}
+
+// helper function: event handler for Job State
+static void StateChanged(object sender, JobStateChangedEventArgs e)
+{
+    Console.WriteLine("Job state changed event:");
+    Console.WriteLine("  Previous state: " + e.PreviousState);
+    Console.WriteLine("  Current state: " + e.CurrentState);
+    switch (e.CurrentState)
+    {
+        case JobState.Finished:
+            Console.WriteLine();
+            Console.WriteLine("Job finished. Please wait for local tasks/downloads");
+            break;
+        case JobState.Canceling:
+        case JobState.Queued:
+        case JobState.Scheduled:
+        case JobState.Processing:
+            Console.WriteLine("Please wait...\n");
+            break;
+        case JobState.Canceled:
+            Console.WriteLine("Job is canceled.\n");
+            break;
+        case JobState.Error:
+            Console.WriteLine("Job failed.\n");
+            break;
+        default:
+            break;
+    }
+}
+// helper method to download the output assets
+static void DownloadAsset(IAsset asset, string outputDirectory)
+{
+    foreach (IAssetFile file in asset.AssetFiles)
+    {
+        file.Download(Path.Combine(outputDirectory, file.Name));
+    }
+}
 }
 
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private static readonly XNamespace ttmlns = "http://www.w3.org/ns/ttml";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here we go");
+            ParseTTML("C:\\Temp\\AATEMP\\Abiola.mp4 - Indexed\\Abiola.mp4.ttml");
+            Console.ReadLine();
+        }
+            private static string ParseTTML(string ttmlFile)
+        {
+            // This will extract all the spoken text from a TTML file into a single string
+            var d = XDocument.Load(ttmlFile);
+            var b = d.Element(ttmlns + "tt")
+                     .Element(ttmlns + "body")
+                     .Element(ttmlns + "div")
+                     .Elements(ttmlns + "p");
+            string t;
+            IEnumerable<XAttribute> segmentAtts;
+            foreach (XElement x in b)
+            {
+                segmentAtts = x.Attributes();
+                foreach (XAttribute x2 in segmentAtts)
+                    t = x2.ToString();
+            }
+            return b.ToString();
+        }
+    }
+    
+}
